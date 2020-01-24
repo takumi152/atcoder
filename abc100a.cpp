764919f7c66436674be25d82adaa0f6210057c4d
takumi152 <takumi152@hotmail.com>
2020-01-24 11:21:23 +0900

    add data 2020-01-24

---
+++ abc100a.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <string>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+typedef long long int ll;
+typedef pair<int, int> Pii;
+
+const ll mod = 1000000007;
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  int a, b;
+  cin >> a >> b;
+
+  if (a <= 8 && b <= 8) cout << "Yay!" << endl;
+  else cout << ":(" << endl;
+
+  return 0;
+}
